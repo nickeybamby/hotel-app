dfbbe9afb8ab7645e14af95a22c14db266930ce6
nicholas <nicholasobazei@gmail.com>
2024-07-29 08:33:41 +0100

    updated the homepage

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,9 +1,19 @@
-import './contact.css'
+import Header from "../../components/Header";
+import HeaderImage from "../../images/gallery/img__contact.png";
+import { BsMessenger } from "react-icons/bs";
+import { IoLogoWhatsapp } from "react-icons";
+
+import "./contact.css";
+import ContactSection from "../../components/ContactSection";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
+  return(
+  <>
+    <Header title="Get In Touch" image={HeaderImage}>
+    Feel free to reach out! We’re here to assist you.
+    </Header>
+  </>
   )
-}
+};
 
-export default Contact
+export default Contact;

8cad779b972f8601f27733c23458d57f6fecdde3
nicholas <nicholasobazei@gmail.com>
2024-08-06 09:39:43 +0100

    updated the homepage

--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,15 +1,33 @@
 import Header from "../../components/Header";
-import HeaderImage from '../../images/gallery/img__gallery.png'
-import './gallery.css'
+import HeaderImage from "../../images/gallery/img__gallery.png";
+import "./gallery.css";
 
 const Gallery = () => {
+  const galleryLength = 12;
+  const images = [];
+
+  for (let i = 1; i <= galleryLength; i++) {
+    images.push(require(`../../images/gallery/gallery${i}.png`));
+  }
+
   return (
     <>
       <Header title="Elegance In Every Corner." image={HeaderImage}>
-      Discover our luxurious suites with breathtaking views. 
+        Discover our luxurious suites with breathtaking views.
       </Header>
+      <section className="gallery">
+        <div className="container mainGallery__container">
+          {images.map((image, index) => {
+            return (
+              <article key={index}>
+                <img src={image} alt={`Gallery Image ${index + 1}`} />
+              </article>
+            );
+          })}
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,11 +1,30 @@
-import './notFound.css'
+import { Link } from "react-router-dom";
+import "./notFound.css";
 
 const NotFound = () => {
   return (
     <>
-      
-    </>
-  )
-}
+      <section className="notfound__section">
+        <div className="container notfound__container">
+          <h1>404</h1>
+          <h2>Page Not Found</h2>
+          <p>
+            It's a shock the page you were looking for is no where to be found.{" "}
+            <br /> Try one of these instead
+          </p>
 
-export default NotFound
+          <div className="notfound__btns">
+            <Link to="/" className="btn notBot">
+              Go Back Home
+            </Link>
+            <Link to="/rooms" className="btn">
+              View Rooms
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default NotFound;
